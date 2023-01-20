c692bd632934aa401bae23dc9a2028e0edc1e192
Kirill Bulekov <kirya.bulekov@mail.ru>
2023-01-20 21:58:05 +0300

    Code palindrome added

---
+++ sem3_task19/Program.cs
@@ -0,0 +1,17 @@
+﻿System.Console.Write("Введите пятизначное число: ");
+string? abcde = Console.ReadLine();
+if (abcde!.Length == 5)
+{
+    if (abcde[0] == abcde[4] && abcde[1] == abcde[3])
+    {
+        System.Console.Write($"Число {abcde} является палиндромом");
+    }
+    else
+    {
+        System.Console.Write($"Число {abcde} не является палиндромом");
+    }
+}
+else
+{
+    System.Console.WriteLine("Вы ввели не пятизначное число");
+}
