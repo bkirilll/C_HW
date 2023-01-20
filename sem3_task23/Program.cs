5bf1341379bb77b761ed9632dd375e687665761b
Kirill Bulekov <kirya.bulekov@mail.ru>
2023-01-20 22:25:10 +0300

    Code cube added

---
+++ sem3_task23/Program.cs
@@ -0,0 +1,8 @@
+﻿System.Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= N; i++)
+{
+    int cube = i * i * i;
+    System.Console.Write($"{cube}, ");
+}
