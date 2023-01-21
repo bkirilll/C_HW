60fb41c57b3d221b7b7e9d34a4ba18ad80d44eab
Kirill Bulekov <kirya.bulekov@mail.ru>
2023-01-21 14:27:09 +0300

    Code weekday/weekend added

---
+++ sem2_task15/Program.cs
@@ -0,0 +1,15 @@
+﻿System.Console.Write("Введите день недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day > 0 && day < 6)
+{
+    System.Console.WriteLine("Этот день не является выходным");
+}
+else if (day == 6 || day == 7)
+{
+    System.Console.WriteLine("Этот день является выходным");
+}
+else
+{
+    System.Console.WriteLine("Вы не ввели день недели");
+}
