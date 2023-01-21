e7b3a6cbc3fe79990588fb4b8c49e98e95001906
Kirill Bulekov <kirya.bulekov@mail.ru>
2023-01-21 14:20:49 +0300

    Code 3rd number added

---
+++ sem2_task13/Program.cs
@@ -0,0 +1,15 @@
+﻿System.Console.Write("Введите любое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 99)
+{
+    while (num > 999)
+    {
+        num = num / 10;
+    }
+    System.Console.Write($"Третья цифра = {num % 10}");
+}
+else
+{
+    System.Console.WriteLine("Третьей цифры нет");
+}
