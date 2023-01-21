3282f83ee9ff9ac97a2d14c8dc727a2461b70a84
Kirill Bulekov <kirya.bulekov@mail.ru>
2023-01-21 14:11:55 +0300

    Code 2nd number added

---
+++ sem2_task10/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 99 && num < 1000)
+{
+    System.Console.WriteLine($"Вторая цифра числа {num} = {(num / 10) % 10} ");
+}
+else
+{
+    System.Console.WriteLine("Вы ввели не трехзначное число");
+}
