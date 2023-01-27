838d14124d64067da029469bd5b79fae628de5ec
Kirill Bulekov <kirya.bulekov@mail.ru>
2023-01-27 19:54:49 +0300

    Exponentiation code added

---
+++ sem4_task25/Program.cs
@@ -0,0 +1,35 @@
+﻿int InputNumber(string str)
+{
+    int number;
+    string text;
+    while (true)
+    {
+        Console.Write(str);
+        text = Console.ReadLine();
+        if (int.TryParse(text, out number))
+        {
+            break;
+        }
+        System.Console.WriteLine("Введено не корректное число");
+    }
+    return number;
+
+}
+
+int a = InputNumber("Введите число А: ");
+int b = InputNumber("Введите число Б: ");
+
+void Exponentiation(int a, int b)
+{
+    int answer = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        answer = answer * a;
+    }
+    System.Console.WriteLine(answer);
+
+}
+
+Exponentiation(a, b);
+
+
