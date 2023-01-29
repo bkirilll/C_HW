f79779ba3a15ac19d681c8e9a7f1767223e5d7ca
Kirill Bulekov <kirya.bulekov@mail.ru>
2023-01-29 22:22:29 +0300

    Array returns added

--- sem4_task29/Program.cs
+++ sem4_task29/Program.cs
@@ -1,12 +1,12 @@
-﻿void Metod(int count)
+﻿int Metod(int count)
 {
     int[] array = new int[8];
     for (int i = 0; i < array.Length; i++)
     {
         Random rand = new Random();
         array[i] = rand.Next(0, 50);
+        System.Console.Write($"{array[i]}, ");
     }
-    System.Console.Write("[" + string.Join(", ", array) + "]");
+    return array[0];
 }
-
 Metod(1);
