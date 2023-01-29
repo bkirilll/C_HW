b4cccec0561cdcbad11d858f4fc361e53e155560
Kirill Bulekov <kirya.bulekov@mail.ru>
2023-01-29 19:01:01 +0300

    Code MakeArray added

---
+++ sem4_task29/Program.cs
@@ -0,0 +1,12 @@
+﻿void Metod(int count)
+{
+    int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Random rand = new Random();
+        array[i] = rand.Next(0, 50);
+    }
+    System.Console.Write("[" + string.Join(", ", array) + "]");
+}
+
+Metod(1);
