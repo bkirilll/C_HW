ef39b5cf161df14c1b7577055a357e518f366770
Kirill Bulekov <kirya.bulekov@mail.ru>
2023-01-31 19:23:15 +0300

    Sum of odd numbers added

---
+++ sem5_task36/Program.cs
@@ -0,0 +1,31 @@
+﻿int[] arrayNumbers(int size, int leftRange, int rightRange)
+{
+    int[] arr = new int[size];
+    Random rand = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return arr;
+}
+
+void SumOfOddNumbers(int[] arr)
+{
+    int sum = 0;
+    for (int i = 1; i < arr.Length; i += 2)
+    {
+        sum = sum + arr[i];
+    }
+    System.Console.WriteLine(sum);
+}
+
+void Print(int[] arr)
+{
+    System.Console.WriteLine("[" + string.Join(", ", arr) + "]");
+}
+
+
+
+int[] array = arrayNumbers(4, -20, 20);
+Print(array);
+SumOfOddNumbers(array);
