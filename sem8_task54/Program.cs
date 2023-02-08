8fa919a477c1dc87a44d607a46e223ddb0ac4fa6
Kirill Bulekov <kirya.bulekov@mail.ru>
2023-02-08 23:36:39 +0300

    Code SortedMatrix added

---
+++ sem8_task54/Program.cs
@@ -0,0 +1,72 @@
+﻿int[,] FillMatrix(int rows, int cols)
+{
+    int[,] matr = new int[rows, cols];
+    Random rand = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            matr[i, j] = rand.Next(0, 10);
+        }
+    }
+    return matr;
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write(matr[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int InputNumber(string str)
+
+{
+    int number;
+    string? text;
+    while (true)
+    {
+        Console.Write(str);
+        text = Console.ReadLine();
+        if (int.TryParse(text, out number))
+        {
+            break;
+        }
+        System.Console.WriteLine("Введено не корректное число");
+    }
+    return number;
+
+}
+
+void SortedMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            for (int k = 0; k < matr.GetLength(1) - 1; k++)
+            {
+                if (matr[i, k] < matr[i, k + 1])
+                {
+                    int temp = matr[i, k + 1];
+                    matr[i, k + 1] = matr[i, k];
+                    matr[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+
+int m = InputNumber("Введите количество строк: ");
+int n = InputNumber("Введите количество столбцов: ");
+int[,] matrix = FillMatrix(m, n);
+PrintMatrix(matrix);
+System.Console.WriteLine("Сортированная матрица: ");
+SortedMatrix(matrix);
+PrintMatrix(matrix);
