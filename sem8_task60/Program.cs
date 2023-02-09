7f74e46c7e5b20b94b7e1ce157d43d463d25c792
Kirill Bulekov <kirya.bulekov@mail.ru>
2023-02-09 18:05:04 +0300

    Code CubeMatrix added

---
+++ sem8_task60/Program.cs
@@ -0,0 +1,32 @@
+﻿int[,,] FillMatrix(int m, int n, int h)
+{
+    int[,,] matr = new int[m*n*h];
+    Random rand = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            for (int k = 0; k < h; k++)
+                matr[i, j, k] = rand.Next(10, 100);
+        }
+    }
+    return matr;
+}
+
+void PrintMatrix(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i, j, k]}{(i, j, k)} ");
+            }
+            System.Console.WriteLine();
+        }
+    }
+}
+
+int[,,] cube = FillMatrix(2, 2, 2);
+PrintMatrix(cube);
