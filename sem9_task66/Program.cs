613ad718cd007ca6775abf4e306c954bac0433b2
Kirill Bulekov <kirya.bulekov@mail.ru>
2023-02-11 14:20:38 +0300

    Code SumRec added

---
+++ sem9_task66/Program.cs
@@ -0,0 +1,10 @@
+﻿int Sum(int M, int N)
+{
+    if (M == 0) return (N * (N + 1)) / 2;           
+    else if (N == 0) return (M * (M + 1)) / 2;       
+    else if (M == N) return M;                      
+    else if (M < N) return N + Sum(M, N - 1); 
+    else return N + Sum(M, N + 1);
+}
+
+System.Console.WriteLine(Sum(4, 8));
