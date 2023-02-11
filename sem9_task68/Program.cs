8432add72e277b6b9d374c94d58b346ae9469e2a
Kirill Bulekov <kirya.bulekov@mail.ru>
2023-02-11 14:14:17 +0300

    Code Ackermann added

---
+++ sem9_task68/Program.cs
@@ -0,0 +1,10 @@
+﻿int AckermannFunction(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if(m > 0 && n ==0) return AckermannFunction(m-1, 1);
+    if(m > 0 && n > 0) return AckermannFunction(m-1, AckermannFunction(m, n-1));
+    return 0;
+}
+
+int N = AckermannFunction(3, 2);
+Console.WriteLine($"Ackermann(m,n) = {N}");
